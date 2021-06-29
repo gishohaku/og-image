2aa37808b2de6d5cba8d16a45a39cdb3abc5cbbc
mottox2 <y.mottox2@gmail.com>
2021-06-30 02:24:00 +0900

    Add watch script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 exports.handler = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World!";
+  let message = req.query.message || req.body.message || "Functions!";
   res.status(200).send(message);
 };
