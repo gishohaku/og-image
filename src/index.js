6483488fdcb9db9d3804ae97eab84dd3c88ba2b6
mottox2 <y.mottox2@gmail.com>
2021-07-03 02:22:53 +0900

    Deploy to cloud functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 const capture = async (html) => {
   // FIXME: Support other OS
-  const browser = await puppeteer.launch({
-    executablePath:
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-  });
+  const browser = await chromium.puppeteer.launch(
+    process.env.NODE_ENV === "production"
+      ? {
+          args: chromium.args,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath,
+          headless: chromium.headless,
+          ignoreHTTPSErrors: true,
+        }
+      : {
+          executablePath:
+            "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+        }
+  );
   const page = await browser.newPage();
   await page.setContent(html);
   await page.setViewport({ width: 1200, height: 630 });
