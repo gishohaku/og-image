f169d208a6aa319525df8cad500ae7624226566a
mottox2 <y.mottox2@gmail.com>
2021-07-02 23:54:40 +0900

    Get html screenshot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const puppeteer = require("puppeteer-core");
 
-exports.handler = async (req, res) => {
+const capture = async (html) => {
   // FIXME: Support other OS
   const browser = await puppeteer.launch({
     executablePath:
       "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   });
   const page = await browser.newPage();
-  await page.goto("https://gishohaku.dev/");
+  await page.setContent(html);
   await page.setViewport({ width: 1200, height: 630 });
   const file = await page.screenshot();
 
   await browser.close();
+  return file;
+};
+
+exports.handler = async (req, res) => {
+  const html = "<h1>Hello World!</h1>";
+  const file = await capture(html);
 
   res.statusCode = 200;
   res.setHeader("Content-Type", `image/png`);
